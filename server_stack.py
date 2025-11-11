75d9a85b2ff7134944097e8ef981bd7270d4689c
CrystalNicole3 <RITT4342@stthomas.edu>
2025-11-11 22:32:27 +0000

    AWS CDK project for Infrastructure as Code assignment

---
+++ server_stack.py
@@ -0,0 +1,7 @@
+from aws_cdk import Stack
+from constructs import Construct
+
+class ServerStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        # Placeholder: resources already deployed in AWS
